909d5d104c07f2abbc65ca226fec8a38720773a2
mahakamal-e <mahakamal09@gmail.com>
2023-11-28 15:40:58 -0800

    fix results

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-for char_ in range(ord('z'), ord('a') - 1, -1):
-    print('{:c}{:c}'.format((char_), (char_ - 33)), end='')
+for i in range(122, 96, -1):
+    if(i % 2 != 0):
+        i = i - 32
+    print("{}".format(chr(i)), end="")
