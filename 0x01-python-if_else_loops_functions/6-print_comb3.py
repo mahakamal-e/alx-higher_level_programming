57c84502236e74b54bb8f429568dcf98ba2c4678
mahakamal-e <mahakamal09@gmail.com>
2023-11-28 11:14:03 -0800

    print numbes

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for start_ in range(10):
-    for end_i in range(start_, 100):
-            if start_ == 8 and end_i == 9:
-                print("{:d}{:d}".format(start_, end_i))
-            else:
-                print("{:d}{:d}".format(start_, end_i), end=(", "))
+    for end_i in range(start_ + 1, 10):
+        if start_ == 8 and end_i == 9:
+            print("{:02d}{:02d}".format(start_, end_i))
+        else:
+            print("{:02d}{:02d}".format(start_, end_i), end=", ")
