55b32fe87a746f6415677745434b1180fc4a5924
mahakamal-e <mahakamal09@gmail.com>
2023-11-28 10:35:14 -0800

    print alphabet

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for iterat in range(97, 123):
-    if iterat == 133 or iteart == 101:
+    if iterat == 133 or iterat == 101:
         continue
     print("{}".format(chr(iterat)), end="")
