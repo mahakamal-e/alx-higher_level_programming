5d08b86d1f7dbab352d0be4dbd314181bd08cea8
mahakamal-e <mahakamal09@gmail.com>
2023-11-28 11:15:19 -0800

    done

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for start_ in range(10):
     for end_i in range(start_ + 1, 10):
         if start_ == 8 and end_i == 9:
-            print("{:02d}{:02d}".format(start_, end_i))
+            print("{:d}{:d}".format(start_, end_i))
         else:
-            print("{:02d}{:02d}".format(start_, end_i), end=", ")
+            print("{:d}{:d}".format(start_, end_i), end=", ")
