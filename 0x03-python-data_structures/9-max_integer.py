a8c4e098a69a44b305e72bf817f6c36629fd5722
mahakamal-e <mahakamal09@gmail.com>
2023-12-02 13:05:37 -0800

    print max

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    else:
+        copy_list = my_list.copy()
+        copy_list.sort()
+        return copy_list[-1]
