133a63c3445ca20612a839e07febfde3a3d08754
mahakamal-e <mahakamal09@gmail.com>
2023-12-19 09:35:07 -0800

    init tha square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''Square class'''
+
+
+class Square:
+    ''' Define square class '''
+
+    def __init__(self, size):
+        """
+        Initilization of the class.
+
+        Args:
+            size: length of the square
+        """
+        self.__size = size
