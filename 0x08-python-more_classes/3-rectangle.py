e342100e264822e4dd6176e14cd390bbc8710a34
mahakamal-e <mahakamal09@gmail.com>
2024-01-04 14:29:15 -0800

    edit task 4

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -66,23 +66,23 @@ class Rectangle:
 
     def area(self):
         """ get the rectangle area """
-        return self.__height * self.width
+        return self.height * self.width
 
     def perimeter(self):
         """
         Calculate the perimeter of the rectangle.
         """
 
-        if self.__width == 0 or self.__height == 0:
+        if self.width == 0 or self.height == 0:
             return 0
-        return 2 * (self.__width + self.__height)
+        return 2 * (self.width + self.height)
 
     def __str__(self):
         """ print the rectangle with the character #"""
-        if self.__width == 0 or self.__height == 0:
+        if self.width == 0 or self.height == 0:
             return ''
 
         rectangle_str = ''
-        for _ in range(self.__height):
-            rectangle_str += '#' * self.__width + '\n'
+        for _ in range(self.height):
+            rectangle_str += '#' * self.width + '\n'
         return rectangle_str.rstrip('\n')
