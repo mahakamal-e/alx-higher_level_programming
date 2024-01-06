441b11cd6c7f81578f7e00033e4edfd9af5d91de
mahakamal-e <mahakamal09@gmail.com>
2024-01-06 13:59:16 -0800

    done

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -7,7 +7,5 @@ class LockedClass:
     def __setattr__(self, name, value):
         """ definr method setter called whenever
         an attribute is set on an object """
-        if name == "first_name":
-            super().__setattr__(name, value)
-        else:
-            raise AttributeError("'LockedClass' object has no attribute '{}'".format(name))
+
+        __slots__ = "first_name"
