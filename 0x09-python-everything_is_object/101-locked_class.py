4896a78a8d419f774ff2b473f85fd73a207aece6
mahakamal-e <mahakamal09@gmail.com>
2024-01-06 14:01:03 -0800

    done

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -4,8 +4,4 @@
 
 class LockedClass:
     """ define LockedClass """
-    def __setattr__(self, name, value):
-        """ definr method setter called whenever
-        an attribute is set on an object """
-
-        __slots__ = "first_name"
+    __slots__ = "first_name"
