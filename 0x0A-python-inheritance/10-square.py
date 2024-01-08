276c47e0a5ee4d50cd426e074906244a9fde2872
mahakamal-e <mahakamal09@gmail.com>
2024-01-08 15:48:42 -0800

    task 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Define Square Module """
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Implement class Square """
+    def __init__(self, size):
+        """ Initialize a new instancle from Square class """
+        self.integer_validator('size', size)
+        super().__init__(size, size)
+        self.__size = size
