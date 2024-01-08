572d4b53ba33862d464ada544bc3f5e93c7508a6
mahakamal-e <mahakamal09@gmail.com>
2024-01-08 11:24:44 -0800

    Task 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" define function called inherits_from """
+
+
+def inherits_from(obj, a_class):
+    """
+     function that returns True if the object is an instance
+     otherwise returns False
+     """
+     return isinstance(obj, a_class) and type(obj) != a_class
