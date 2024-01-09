298c496806201f152b764a7f5f95a6bf5172b95b
mahakamal-e <mahakamal09@gmail.com>
2024-01-09 15:51:32 -0800

    10-student

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Class Student Module """
+
+
+class Student:
+    """defines a student by"""
+    def __init__(self, first_name, last_name, age):
+        """ Initialize  new instance """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance"""
+        try:
+            for attr_ in attrs:
+                if type(attr_) is not str:
+                    return self.__dict__
+        except Exception:
+            return self.__dict__
+        my_dict = {}
+        for key in attrs:
+            if hasattr(self, key):
+                my_dict[key] = getattr(self, key)
+        return my_dict
