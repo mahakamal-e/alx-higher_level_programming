2ddbb2ac364de8f7e86db2db54d9c6694bf7734e
mahakamal-e <mahakamal09@gmail.com>
2024-01-08 16:07:41 -0800

    Task 100

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -7,6 +7,7 @@ class MyInt(int):
     def __eq__(self, other):
         """ reverse == operator """
         return not super().__eq__(other)
+
     def __ne__(self, other):
         """ reverse != operator """
         return not super().__ne__(other)
