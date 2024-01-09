6f6650f7da170dd5a0d7f3660a5b4e1bd0438f1e
mahakamal-e <mahakamal09@gmail.com>
2024-01-09 15:11:38 -0800

    add_item

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" 7-add_item Module """
+import sys
+import os
+
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+filename = "add_item.json"
+
+
+if __name__ == '__main__':
+    filename = "add_item.json"
+    python_list = sys.argv[1:]
+    if os.path.exists(filename):
+        python_list = load_from_json_file(filename) + python_list
+    save_to_json_file(python_list, filename) 
