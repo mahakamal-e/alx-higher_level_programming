0552dcecb63b59de0bd774e756dff56e84033377
mahakamal-e <mahakamal09@gmail.com>
2024-02-14 17:40:19 -0800

    update

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,6 +3,6 @@
 SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows
 FROM tv_genres
 JOIN tv_show_genres
-ON tv_genres.id = tv_show_genres.genre_id
-GROUP BY tv_genres.genre
+ON id = genre_id
+GROUP BY tv_genres.name
 ORDER BY number_of_shows DESC;
