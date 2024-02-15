5229675388d522a8a51d78fdf13a95e9a1cf85c5
mahakamal-e <mahakamal09@gmail.com>
2024-02-14 17:52:05 -0800

    task 16

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,9 +1,9 @@
 -- a script that lists all shows, and all genres linked to that show.
-SELECT ts.title, tg.name
+SELECT t.title, tgs.name
 FROM
-tv_shows AS t
-LEFT JOIN tv_show_genres AS tg
-ON tg.show_id = t.id
-LEFT JOIN tv_genres AS tgs
-ON tg.genre_id = tgs.id
-ORDER BY t.title, tgs.name;
+  tv_shows AS t
+  LEFT JOIN tv_show_genres AS tg
+  ON tg.show_id = t.id
+  LEFT JOIN tv_genres AS tgs
+  ON tg.genre_id = tgs.id
+  ORDER BY t.title, tgs.name;
