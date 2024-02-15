9e9f180bd1ebd78a7adf111617fe71a4666d735e
mahakamal-e <mahakamal09@gmail.com>
2024-02-15 13:24:23 -0800

    add

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,2 @@
 -- a script that converts hbtn_0c_0 database to UTF8.
-ALTER TABLE first_table CONVERT TO CHARACTER
-SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
