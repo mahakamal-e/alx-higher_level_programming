4ab69e774a3e4103480172df2ef74c553f9fa9dd
mahakamal-e <mahakamal09@gmail.com>
2024-02-15 18:08:39 -0800

    edit

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -3,7 +3,8 @@ SELECT tv_shows.title
 FROM tv_shows
 WHERE id NOT IN (
 	SELECT tv_show_genres.show_id
-	FROM tv_show_genres
-	WHERE tv_show_genres.genre_id = (SELECT id FROM tv_genres WHERE tv_genres.name = 'Comedy')
-)
-ORDER BY tv_shows.title ASC;
+	FROM tv_genres
+	INNER JOIN tv_show_genres
+	ON tv_show_genres.genre_id = tv_genres.id
+	WHERE tv_genres.name = 'Comedy')
+ORDER BY tv_shows.title
