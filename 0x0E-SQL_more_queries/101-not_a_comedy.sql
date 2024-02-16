1e3dfb31c2d51b0890da1b475b55c274586e0e2a
mahakamal-e <mahakamal09@gmail.com>
2024-02-15 18:01:27 -0800

    ad

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -2,9 +2,8 @@
 SELECT title
 FROM tv_shows
 WHERE id NOT IN (
-    -- Subquery from Step 1
-    SELECT show_id
-    FROM tv_show_genres
-    WHERE genre_id = (SELECT id FROM tv_genres WHERE name = 'Comedy')
+	SELECT show_id
+	FROM tv_show_genres
+	WHERE genre_id = (SELECT id FROM tv_genres WHERE name = 'Comedy')
 )
 ORDER BY title ASC;
