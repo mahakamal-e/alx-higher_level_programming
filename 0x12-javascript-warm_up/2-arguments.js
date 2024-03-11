b33b4980b364c18a53fce22a153c6f987b2c2c3c
mahakamal-e <mahakamal09@gmail.com>
2024-03-11 10:50:24 -0700

    edit

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -6,6 +6,6 @@ if (argNum === 2) {
   console.log('No argument');
 } else if (argNum === 3) {
   console.log('Argument found');
-} else if (argNum === 4) {
+} else {
   console.log('Arguments found');
 }
