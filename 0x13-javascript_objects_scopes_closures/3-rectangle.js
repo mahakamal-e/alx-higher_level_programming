643132ede865074e8248d3a4073797fe517c576e
mahakamal-e <mahakamal09@gmail.com>
2024-03-12 11:39:20 -0700

    task 3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,10 +1,14 @@
 #!/usr/bin/node
-
 class Rectangle {
   constructor(w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
+    }
+  }
+  print() {
+    for (let i = 0; i < height; i++) {
+      console.log('X'.repeat(this.width));
     }
   }
 }
