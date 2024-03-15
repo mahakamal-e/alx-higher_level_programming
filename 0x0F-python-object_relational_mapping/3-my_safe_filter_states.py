909ecf19f2ff8dfbf5408854f59b6cba14e1b2d0
mahakamal-e <mahakamal09@gmail.com>
2024-03-15 22:18:48 +0000

    task 3 git push

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Module for writing a Script that takes in an argument
+and displays all values
+"""
+
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db_connection = MySQLdb.connect(host="localhost",
+                                    port=3306,
+                                    user=sys.argv[1],
+                                    passwd=sys.argv[2],
+                                    db=sys.argv[3])
+    cursor = db_connection.cursor()
+    cursor.execute("SELECT * FROM states WHERE BINARY name = '{}'"
+                   "ORDER BY id ASC".format(sys.argv[4]))
+    result = cursor.fetchall()
+
+    for state in result:
+        print(state)
+
+    cursor.close()
+    db_connection.close()
