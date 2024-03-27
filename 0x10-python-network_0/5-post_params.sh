3102b7f959a5d9c06058441946380842f4b0507e
mahakamal-e <mahakamal09@gmail.com>
2024-03-27 13:15:27 -0700

    add task 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes a URL, sends a POST request, displays the body of the response.
-curl -s -X POST -d "email=$email&subject=$subject" "$1"
+curl -s -X POST -d "email="test@gmail.com"&subject="I will always be here for PLD" "$1"
