ace7196bdda46f7cec034f453aa1eea097c902f4
mahakamal-e <mahakamal09@gmail.com>
2024-03-28 13:11:12 -0700

    fix errors

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -9,6 +9,6 @@ import sys
 if __name__ == "__main__":
 
     url = sys.argv[1]
-    response = request.get(url)
+    response = requests.get(url)
     header = response.headers.get("X-Request-Id")
     print(header)
