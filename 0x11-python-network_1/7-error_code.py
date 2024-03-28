0cca3e827a29432013f40550431e8786dd4ffa88
mahakamal-e <mahakamal09@gmail.com>
2024-03-28 13:21:54 -0700

    task 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Python script that takes in a URL, sends a request to the URL &
+displays the body of the response."""
+
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    response = requests.get(url)
+    print(response.text)
+
+    if response.status_code >= 400:
+        print("Error code: {}".format(response.status_code))
