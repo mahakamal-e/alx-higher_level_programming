b503aac735af05d408f21369e58740d5489160cc
mahakamal-e <mahakamal09@gmail.com>
2024-04-23 15:30:09 -0700

    dd

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,7 +3,7 @@ const request = require('request');
 const url = process.argv[2];
 const fs = require('fs');
 
-request.get(url, (error, response, body) => {
+request(url, (error, response, body) => {
   if (error) console.log(error);
   else {
     fs.writeFile(args[3], body, 'utf-8', (error) => {
