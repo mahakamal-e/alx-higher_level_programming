f8cee13d036810c923b69a82cfbc548c5cd4942d
mahakamal-e <mahakamal09@gmail.com>
2024-04-23 14:37:39 -0700

    dd

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,2 +1,11 @@
 #!/usr/bin/node
-const
+const request = require('request');
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
