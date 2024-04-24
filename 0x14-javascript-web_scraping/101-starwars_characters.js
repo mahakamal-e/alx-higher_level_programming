d7b416564f2061278bb616829b1be2629b667170
mahakamal-e <mahakamal09@gmail.com>
2024-04-23 19:57:06 -0700

    e

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,27 +1,27 @@
 #!/usr/bin/node
 
-const movieId = process.argv[2];
+const args = process.argv;
 const request = require('request');
 
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+const url = `https://swapi-api.alx-tools.com/api/films/${args[2]}`;
 
 request(url, (error, response, body) => {
   if (error) console.log(error);
   else {
-    const movieCharacters = JSON.parse(body).characters;
+    const moviesCharacters = JSON.parse(body).characters;
 
-    const promises = movieCharacters.map((character) => {
+    const promises = moviesCharacters.map((character) => {
       return new Promise((resolve, reject) => {
         request(character, (error, response, body) => {
           if (error) reject(error);
-          else resolve(JSON.parse(body).name.toUpperCase());
+          else resolve(JSON.parse(body).name);
         });
       });
     });
 
-    Promise.all(promises).then((characterNames) => {
-      characterNames.forEach((characterName) => {{
-        console.log(characterName);
+    Promise.all(promises).then((names) => {
+      names.forEach((name) => {
+        console.log(name);
       });
     });
   }
