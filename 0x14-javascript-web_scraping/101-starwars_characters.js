ff5666e9306a9f30ee0c6881efcf938c7c963965
mahakamal-e <mahakamal09@gmail.com>
2024-04-23 19:56:05 -0700

    edit

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -3,27 +3,26 @@
 const movieId = process.argv[2];
 const request = require('request');
 
-const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-    return;
-  }
+request(url, (error, response, body) => {
+  if (error) console.log(error);
+  else {
+    const movieCharacters = JSON.parse(body).characters;
 
-  const movie = JSON.parse(body);
-  const characters = movie.characters;
-
-  const printCharacterName = (characterUrl) => {
-    request(characterUrl, (error, response, body) => {
-      if (error) {
-        console.error('Error:', error);
-        return;
-      }
-      const character = JSON.parse(body);
-      console.log(character.name);
+    const promises = movieCharacters.map((character) => {
+      return new Promise((resolve, reject) => {
+        request(character, (error, response, body) => {
+          if (error) reject(error);
+          else resolve(JSON.parse(body).name.toUpperCase());
+        });
+      });
     });
-  };
 
-  characters.forEach(printCharacterName);
+    Promise.all(promises).then((characterNames) => {
+      characterNames.forEach((characterName) => {{
+        console.log(characterName);
+      });
+    });
+  }
 });
