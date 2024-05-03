6e4f8ed914c821d86ae7ffecb267393ad54dfed9
mahakamal-e <mahakamal09@gmail.com>
2024-05-02 19:03:45 -0700

    upload files

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $.ajax({
+    url: 'https://swapi-api.alx-tools.com/api/films/?format=json',
+    method: "GET",
+    success: function (data) {
+      $.each(data.results, function (index, film) {
+        $('#list_movies').append(`<li>${film.title}</li>`);
+      });
+    }
+  });
+});
